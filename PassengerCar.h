3edc7702d3af374e66e44c52739ba03adda21a3c
BuggedPotato <92549622+BuggedPotato@users.noreply.github.com>
2024-05-20 00:42:57 +0200

    Update PassengerCar.h

--- PassengerCar.h
+++ PassengerCar.h
@@ -8,9 +8,7 @@ public:
 	PassengerCar( std::string brandArg, std::string modelArg, int productionYearArg, std::string licenseNumberArg, float massArg, int seatsArg ) : CarBase( brandArg, modelArg, productionYearArg, licenseNumberArg, massArg ), seats( seatsArg ) {};
 	PassengerCar( const PassengerCar &carArg );
 
-	virtual std::string getInfo() {
-		return this->brand + " " + this->model + ", rok produkcji: " + std::to_string( this->productionYear ) + ", " + std::to_string(this->seats) + " pasa¿erów, nr rejestracyjny: " + this->licenseNumber;
-	}
+	virtual std::string getInfo();
 	int getSeats();
 	void setSeats( int newSeats );
 };
