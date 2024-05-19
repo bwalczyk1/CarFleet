e22cb3c2bb429cb07b50404b5188442d36d8673c
BuggedPotato <92549622+BuggedPotato@users.noreply.github.com>
2024-05-20 00:39:36 +0200

    Update DeliveryCar.h

--- DeliveryCar.h
+++ DeliveryCar.h
@@ -8,9 +8,7 @@ public:
 	DeliveryCar( std::string brandArg, std::string modelArg, int productionYearArg, std::string licenseNumberArg, float massArg, float maxAuthorisedMassArg ) : CarBase( brandArg, modelArg, productionYearArg, licenseNumberArg, massArg ), maxAuthorisedMass( maxAuthorisedMassArg ) {};
 	DeliveryCar( const DeliveryCar &carArg );
 
-	virtual std::string getInfo() {
-		return this->brand + " " + this->model + ", rok produkcji: " + std::to_string( this->productionYear ) + ", maks. ³adunek: " + std::to_string(this->getMaxLoad()) + "kg, nr rejestracyjny: " + this->licenseNumber;
-	}
+	virtual std::string getInfo();
 	float getMaxAuthorisedMass();
 	void setMaxAuthorisedMass( float newMaxAuthorisedMass );
 	float getMaxLoad();
