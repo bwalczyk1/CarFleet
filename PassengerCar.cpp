0eeb52b5751daff3b7b427b18dcbafc928a1b8e2
BuggedPotato <92549622+BuggedPotato@users.noreply.github.com>
2024-05-20 00:43:19 +0200

    Update PassengerCar.cpp

--- PassengerCar.cpp
+++ PassengerCar.cpp
@@ -4,6 +4,10 @@ PassengerCar::PassengerCar( const PassengerCar &carArg ) : CarBase( carArg ) {
 	this->seats = carArg.seats;
 }
 
+virtual std::string PassengerCar::getInfo() {
+	return this->brand + " " + this->model + ", rok produkcji: " + std::to_string( this->productionYear ) + ", " + std::to_string(this->seats) + " pasa¿erów, nr rejestracyjny: " + this->licenseNumber;
+}
+
 int PassengerCar::getSeats() {
 	return this->seats;
 }
